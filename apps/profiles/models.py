0fda8cfcd5e5e7ea901334c44b83ec0052eb095c
Stanislav Vilisov <vilisov@steelkiwi.com>
2013-09-26 13:14:48 +0300

    change OneToOneField to user model on settings.AUTH_USER_MODEL

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -1,17 +1,18 @@
-from django.contrib.auth.models import User
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, verbose_name=_('user'), related_name='profile')
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, verbose_name=_('user'), related_name='profile')
 
     def __unicode__(self):
         return self.user.username
 
 
-@receiver(models.signals.post_save, sender=User)
+@receiver(models.signals.post_save, sender=get_user_model())
 def create_profile(sender, created, instance, **kwargs):
     if created:
         Profile.objects.create(user=instance)
